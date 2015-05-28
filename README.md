be6f899ddbb51f479b7522a76c6b3244b9334de3
RobertAKARobin <robertgfthomas@gmail.com>
2015-05-28 16:48:30 -0400

    Added fancy readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Looking to make elements draggable on IE8, mobile devices, and regular computers?
+
+I gotcha covered.
+
+```
+new Draggable(document.getElementById("hello"));
+```
--- draggable.js
+++ draggable.js
@@ -55,7 +55,7 @@ Draggable.prototype = {
 }
 
 window.onload = function(){
-  var tags = document.querySelectorAll(".tags>*");
+  var tags = document.querySelectorAll(".draggable");
   var listeners = [];
   for(var x = tags.length - 1; x >= 0; x--){
     listeners.push(new Draggable(tags[x]));
